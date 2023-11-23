ac483e160fe67d2867d83f0526fd5bb664abc3c8
dwiaffan30 <148044581+dwiaffan30@users.noreply.github.com>
2023-11-23 19:58:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+// MATH ROUND
+let a = 5.2;
+let b = 4.5;
+let c = 4.2;
+
+var round = Math.round(a);
+console.log(round);
+
+var round2 = Math.round(b);
+console.log(round2);
+
+var round3 = Math.round(c);
+console.log(round3);
+
+// MATH ROUND
+var atas = Math.ceil(a);
+console.log(atas);
+
+var atas2 = Math.ceil(b);
+console.log(atas2);
+
+var atas3 = Math.ceil(c);
+console.log(atas3);
+
+// MATH FLOOR
+var bawah = Math.floor(a);
+console.log(bawah);
+
+var bawah2 = Math.floor(b);
+console.log(bawah2);
+
+var bawah3 = Math.floor(c);
+console.log(bawah3);
+
+
+// MATH TRUNC
+let z = 3.9050;
+let s = 4.0945;
+let q = 8.0078;
+
+var trunc = Math.trunc(z);
+console.log(trunc);
+
+var trunc2 = Math.trunc(s);
+console.log(trunc2);
+
+var trunc3 = Math.trunc(q);
+console.log(trunc3);
+
+// MATH POW
+let pangkat = Math.pow( 4 , 2);
+console.log(pangkat);
+
+let pangkat2 = Math.pow( 8 , 5);
+console.log(pangkat2);
+
+let pangkat3 = Math.pow( 3 , 2);
+console.log(pangkat3);
+
+
+// MATH SQRT
+let akar = Math.sqrt(81);
+console.log(akar);
+
+let akar2 = Math.sqrt(16);
+console.log(akar2);
+
+let akar3 = Math.sqrt(9);
+console.log(akar3);

51d3c35ab5d183834c04c33d8aab86406f5807cc
dwiaffan30 <148044581+dwiaffan30@users.noreply.github.com>
2023-12-03 08:09:15 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,69 +1,43 @@
-// MATH ROUND
-let a = 5.2;
-let b = 4.5;
-let c = 4.2;
+let konfirmasi = confirm("Mau main tebak angka ?");
+let penjelasan = confirm("tebak angka diantara 1-10");
 
-var round = Math.round(a);
-console.log(round);
+while (konfirmasi == true){
+    let angkarandom = Math.floor(Math.random() * 10 + 1 );
+    console.log(angkarandom);
+    let masukkanuser = prompt("Masukkan Angka Tebakan");
+    if(angkarandom == masukkanuser ){
+        alert("Selamat anda berhasil ");
+    }else if(masukkanuser < angkarandom){ 
+        alert("angka terlau rendah")
+        konfirmasi = confirm ("yakin nih gak mau lanjut ?")
+    }
+    else if(masukkanuser > angkarandom){ 
+    alert("ketinggian mas bro")
+    konfirmasi = confirm ("masa udah nyerah aja , lanjut gak ?")
+    }
+}    
+   
 
-var round2 = Math.round(b);
-console.log(round2);
+    
 
-var round3 = Math.round(c);
-console.log(round3);
+    while(masukkan != angkarandom && konfirmasi == true){
+    let masukkanuser = perseInt(prompt("Masukkan Angka Tebakan !!"))
+    if(angkarandom == masukkanuser ){
+        alert("Selamat anda berhasil ");    
+    }else if(masukkanuser < angkarandom){ 
+        alert("angka terlalu rendah")
+        konfirmasi = confirm ("yakin nih gak mau lanjut ?")
+    }
+    else if(masukkanuser > angkarandom){ 
+    alert("ketinggian mas bro")
+    konfirmasi = confirm ("masa udah nyerah aja , lanjut gak ?")
+    }else{
+        konfirmasi = false;
+    }
 
-// MATH ROUND
-var atas = Math.ceil(a);
-console.log(atas);
-
-var atas2 = Math.ceil(b);
-console.log(atas2);
-
-var atas3 = Math.ceil(c);
-console.log(atas3);
-
-// MATH FLOOR
-var bawah = Math.floor(a);
-console.log(bawah);
-
-var bawah2 = Math.floor(b);
-console.log(bawah2);
-
-var bawah3 = Math.floor(c);
-console.log(bawah3);
-
-
-// MATH TRUNC
-let z = 3.9050;
-let s = 4.0945;
-let q = 8.0078;
-
-var trunc = Math.trunc(z);
-console.log(trunc);
-
-var trunc2 = Math.trunc(s);
-console.log(trunc2);
-
-var trunc3 = Math.trunc(q);
-console.log(trunc3);
-
-// MATH POW
-let pangkat = Math.pow( 4 , 2);
-console.log(pangkat);
-
-let pangkat2 = Math.pow( 8 , 5);
-console.log(pangkat2);
-
-let pangkat3 = Math.pow( 3 , 2);
-console.log(pangkat3);
+    console.log(masukkanuser)
+    
+}
 
 
-// MATH SQRT
-let akar = Math.sqrt(81);
-console.log(akar);
 
-let akar2 = Math.sqrt(16);
-console.log(akar2);
-
-let akar3 = Math.sqrt(9);
-console.log(akar3);
